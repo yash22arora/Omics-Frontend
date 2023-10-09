405690b889513a8d11cde460dc9563bbca567aba
Yashvardhan Arora <yash22arora@gmail.com>
2023-10-09 12:49:02 +0530

    Add accepted/rejected message in UploadBox
    Signed-off-by: Yashvardhan Arora <yash22arora@gmail.com>

--- client/src/components/UploadBox.tsx
+++ client/src/components/UploadBox.tsx
@@ -8,7 +8,13 @@ const UploadBox: React.FC<{
     // Do something with the files
     onUpload(acceptedFiles[0]);
   }, []);
-  const { getRootProps, getInputProps, isDragActive } = useDropzone({
+  const {
+    getRootProps,
+    getInputProps,
+    isDragActive,
+    acceptedFiles,
+    fileRejections,
+  } = useDropzone({
     onDrop,
     multiple: false,
     accept: {
@@ -20,18 +26,31 @@ const UploadBox: React.FC<{
   });
 
   return (
-    <div
-      {...getRootProps()}
-      className="p-6 border border-dashed my-4 cursor-pointer text-center"
-    >
-      <input {...getInputProps()} />
-      {isDragActive ? (
-        <p>Drop the files here ...</p>
-      ) : (
-        <div className="flex flex-col items-center justify-center gap-2 opacity-70">
-          <span>Drag and drop the CSV file here</span>
-          <span className="opacity-50">- - - or - - -</span>
-          <span>Click to select file</span>
+    <div {...getRootProps()} className="flex flex-col gap-2  my-4">
+      <div className="p-6 border-2 border-dashed cursor-pointer text-center">
+        <input {...getInputProps()} />
+        {isDragActive ? (
+          <p>Drop the files here ...</p>
+        ) : (
+          <div className="flex flex-col items-center justify-center gap-2 opacity-70">
+            <span>Drag and drop the CSV file here</span>
+            <span className="opacity-50">- - - or - - -</span>
+            <span>Click to select file</span>
+          </div>
+        )}
+      </div>
+      {acceptedFiles.length > 0 && (
+        <div className="mt-4">
+          <span className="font-medium text-green-500">Selected File:</span>
+          <span className="ml-2">{acceptedFiles[0].name}</span>
+        </div>
+      )}
+      {acceptedFiles.length === 0 && fileRejections.length > 0 && (
+        <div className="mt-4">
+          <span className="font-medium text-red-500">
+            File rejected - Only .csv file is permissible. Multiple files upload
+            not allowed.
+          </span>
         </div>
       )}
     </div>
